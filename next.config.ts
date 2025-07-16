4d13713a30e54fc8f973d539bbb21bb5f167d154
Vishvaraj18 <vishvaraj.dgkgr22@sinhgad.edu>
2025-07-17 02:02:04 +0530

    Deployment issues

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com', // Add this hostname for Google user avatars
+      // Add any other external image domains you might use
+    ],
+  },
 };
 
 export default nextConfig;
